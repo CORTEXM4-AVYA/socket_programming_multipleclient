3019652289d74a2fc96ff74a51228e04a74a763c
CORTEXM4-AVYA <61452216+CORTEXM4-AVYA@users.noreply.github.com>
2020-06-12 18:06:48 +0530

    Add files via upload

---
+++ server.h
@@ -0,0 +1,24 @@
+#ifndef LIST
+#define LIST
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct ClientNode {
+    int data;
+    struct ClientNode* prev;
+    struct ClientNode* link;
+    char ip[16];
+    char name[31];
+} ClientList;
+
+ClientList *newNode(int sockfd, char* ip) {
+    ClientList *np = (ClientList *)malloc( sizeof(ClientList) );
+    np->data = sockfd;
+    np->prev = NULL;
+    np->link = NULL;
+    strncpy(np->ip, ip, 16);
+    strncpy(np->name, "NULL", 5);
+    return np;
+}
+
+#endif // LIST
